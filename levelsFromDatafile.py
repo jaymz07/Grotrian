bdbde3f9ec9bbf004eaed09b37e28b5ba949bb96
James Bounds <jamesbounds@tamu.edu>
2016-05-18 15:39:13 -0500

    Working formatting options in datafile. Working Example datafiles.

--- levelsFromDatafile.py
+++ levelsFromDatafile.py
@@ -11,34 +11,59 @@ from fractions import Fraction
 
 levelWidth = 0.2
 
-dataFile = 'hydrogen.levels'
+dataFile = 'data_files/Mg-II.levels'
 dataFileSeparator = ','
+
+showElectricDipole =        False
+showElectricQuadrupole =    False
 
 title = 'Energy Level Diagram for ' + dataFile
 scale = 'cm$^{-1}$'
 
 file = open(dataFile,'r')
+minY, maxY = 0, 0
+yMargin = 0.1
 
 levels = []
+transitions = []
 for line in file:
-    if(line[0] == '$'):
+    if(len(line)>=11 and line[0:11] == '$TRANSITION'):
         ln = line.split(dataFileSeparator)
-        for command in ln:
+        transitions.append({'i': int(ln[1]), 'f': int(ln[2])})
+        if(len(ln)>3):
+            for part in ln:
+                command=part.split('=')
+                if(command[0]=='$LABEL'):
+                    transitions[-1]['label']=command[1]
+                elif(command[0]=='$COLOR'):
+                    transitions[-1]['color']=command[1]
+                elif(command[0]=='$SHOW-NM'):
+                    if(command[1].rstrip()=='1' or command[1].rstrip().lower()=='t'):
+                        transitions[-1]['show-nm']=True
+                    else:
+                        transitions[-1]['show-nm']=False
+    elif(line[0] == '$'):
+        ln = line.split(dataFileSeparator)
+        for command in ln:  
             vals = command.split("=")
+            if(len(command)==0):
+                continue
             if(vals[0] == '$TITLE'):
                 title = vals[1]
             elif(vals[0] == '$SCALE'):
                 scale = vals[1]
             else:
                 print('Invalid Command encountered data file' + dataFile)
+                print('---->' + line)
     else:
         ln = line.split(dataFileSeparator)
-        levels.append({'energy' : float(ln[0]), 'label' : ln[1]})
+        if(len(ln[0])==0):
+            continue
+        v=float(ln[0])
+        levels.append({'energy' : v, 'label' : ln[1]})
+        minY = min(v,minY)
+        maxY = max(v,maxY)
 file.close()
-    
-transitions = []
-transitions.append( {'i' : 2, 'f' : 0})
-transitions.append( {'i' : 8, 'f' : 0})
 
 for i in range(0,len(levels)):
     st = levels[i]['label']
@@ -53,6 +78,8 @@ for i in range(0,len(levels)):
         levels[i]['l']=1
     elif('D' in st[1]):
         levels[i]['l']=2
+    elif('F' in st[1]):
+        levels[i]['l']=3
     else:
         levels[i]['l']=-1
     
@@ -62,23 +89,58 @@ for i in range(0,len(levels)):
         xstart = 2
     elif(levels[i]['l'] == 2):
         xstart = 3
+    elif(levels[i]['l'] == 3):
+        xstart = 4
     levels[i]['xstart']=xstart
-"""
-for i in range(0,len(levels)):
-    for j in range(i+1,len(levels)):
-        delta_j = abs(levels[i]['j']-levels[j]['j'])
-        if(delta_j == Fraction(1,1) or delta_j == Fraction(0,1)):
-            if()
-"""
+
+if(showElectricDipole):
+    for i in range(0,len(levels)):
+        for j in range(i+1,len(levels)):
+            delta_j = abs(levels[i]['j']-levels[j]['j'])
+            if(delta_j == Fraction(1,1) or delta_j == Fraction(0,1)):
+                if(abs(levels[i]['l']-levels[j]['l'])==1):
+                    transitions.append({'i' : i, 'f' : j})
+                    
+if(showElectricQuadrupole):
+    for i in range(0,len(levels)):
+        for j in range(i+1,len(levels)):
+            delta_j = abs(levels[i]['j']-levels[j]['j'])
+            if(delta_j == Fraction(2,1) or delta_j == Fraction(0,1)):
+                if(abs(levels[i]['l']-levels[j]['l'])==2):
+                    transitions.append({'i' : i, 'f' : j})
+
+def nmString(transition):
+    return str(int(1239.8393589807376/abs(levels[trans['i']]['energy'] - levels[trans['f']]['energy']))) + 'nm'
+
+for trans in transitions:
+    if(not trans.has_key('label')):
+        if(trans.has_key('show-nm') and not trans['show-nm']):
+            continue
+        else:
+            trans['label']=nmString(trans)
+    elif(trans.has_key('show-nm') and trans['show-nm']):
+        trans['label']=trans['label'] + ' (' + nmString(trans) +')'
+
 for level in levels:
     plt.plot([level['xstart']-levelWidth,level['xstart']+levelWidth],[level['energy'],level['energy']],'-0')
 for level in levels:
     plt.annotate('$'+level['label'].split('_')[0]+ '_{'+ str(level['j']) + '}$',xy=(level['xstart']+levelWidth,level['energy']))
 for t in transitions:
-    plt.arrow(levels[t['i']]['xstart'],levels[t['i']]['energy'],levels[t['f']]['xstart']-levels[t['i']]['xstart'],levels[t['f']]['energy']-levels[t['i']]['energy'],head_width=0.1,head_length=.2,color='red')
-plt.ylim(-1,15)
+    x=levels[t['i']]['xstart']
+    y=levels[t['i']]['energy']
+    dx = levels[t['f']]['xstart']-levels[t['i']]['xstart']
+    dy = levels[t['f']]['energy']-levels[t['i']]['energy']
+    dr = np.sqrt(dx**2+dy**2)
+    headLength, headWidth = 0.0, 0.00
+    tColor = 'red'
+    if(t.has_key('color')):
+        tColor=t['color']
+    plt.arrow(x,y,dx-dx/dr*headLength,dy-dy/dr*headLength,head_width=headWidth,head_length=headLength,color=tColor.rstrip())
+    plt.annotate(t['label'],xy=(x+dx/2,y+dy/2),color=tColor.rstrip())
+yRange = maxY-minY
+plt.ylim(minY-yMargin*yRange,maxY+yMargin*yRange)
 plt.ylabel(scale)
 plt.title(title)
 plt.xlim(0,5)
-plt.xticks([1,2,3],['S','P','D'])
+plt.xticks([1,2,3,4],['S','P','D','F'])
 plt.show()
