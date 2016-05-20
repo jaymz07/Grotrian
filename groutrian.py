f0d08c27d7963af6ef709e3614131e2ed2ac712a
James Bounds <jamesbounds@tamu.edu>
2016-05-20 14:55:32 -0500

    Fixed some input problems. Can now use invalid labels. Began level splitting recognization

--- groutrian.py
+++ groutrian.py
@@ -19,6 +19,8 @@ dataFileSeparator = ','
 showElectricDipole =        False
 showElectricQuadrupole =    False
 
+showSplittings = True
+
 for i in range(0,len(sys.argv)):
     if((sys.argv[i] == '-i' or sys.argv[i]=='--input') and len(sys.argv)>i+1):
         dataFile=sys.argv[i+1]
@@ -40,7 +42,8 @@ for i in range(0,len(sys.argv)):
 
 title = 'Energy Level Diagram for ' + dataFile
 scale = 'cm$^{-1}$'
-termSymbols = ['S','P','D','F','G','H','I','J','K','L','M','N','O','P','Q','R']
+termSymbols = ['S','P','D','F','G','H','I','J','K','L','M','N','O','Q','R']
+labelError = False
 
 file = open(dataFile,'r')
 minY, maxY, minX, maxX = 0, 0, 0, 0
@@ -49,7 +52,12 @@ yMargin = 0.1
 
 levels = []
 transitions = []
+splittings = []
+
+##Read data file
 for line in file:
+    if(len(line)==0):
+        continue
     if(len(line)>=11 and line[0:11] == '$TRANSITION'):
         ln = line.split(dataFileSeparator)
         transitions.append({'i': int(ln[1]), 'f': int(ln[2])})
@@ -89,48 +97,71 @@ for line in file:
 file.close()
 multiplicities=[]
 
+##Parse level properties from label
 for i in range(0,len(levels)):
-    st = levels[i]['label']
-    st=st.split('^')
-    
-    levels[i]['n']=int(st[0])
-    levels[i]['mult']=int(st[1][0])
-    levels[i]['j']=Fraction(st[1].split('_')[-1])
-    if('S' in st[1]):
-        levels[i]['l']=0
-    elif('P' in st[1]):
-        levels[i]['l']=1
-    elif('D' in st[1]):
-        levels[i]['l']=2
-    elif('F' in st[1]):
-        levels[i]['l']=3
-    else:
-        levels[i]['l']=-1
-    
-    if(levels[i]['l'] == 0):
-        xstart = 1
-    elif(levels[i]['l'] == 1):
-        xstart = 2
-    elif(levels[i]['l'] == 2):
-        xstart = 3
-    elif(levels[i]['l'] == 3):
-        xstart = 4
-    maxl=max(maxl,levels[i]['l'])
-    
-    levels[i]['s']=(levels[i]['mult']-1)/2
+    lbl = levels[i]['label']
+    if(lbl.count('^') == 1 and lbl.count('_')==1):
+        st = lbl
+        st=st.split('^')
         
-    levels[i]['xstart']=xstart
-    minX=min(xstart,minX)
-    maxX=max(xstart,maxX)
-    
-    if(levels[i]['mult'] not in multiplicities):
-        multiplicities.append(levels[i]['mult'])
+        levels[i]['n']=int(st[0])
+        levels[i]['mult']=int(st[1][0])
+        levels[i]['j']=Fraction(st[1].split('_')[-1])
+        
+        hasTerm = False
+        for j in range(0,len(termSymbols)):
+            if(termSymbols[j] in st[1]):
+                levels[i]['l'] = j
+                hasTerm = True
+        if(not hasTerm):
+            levels[i]['l']=-1
+        
+        xstart = levels[i]['l']+1
+        maxl=max(maxl,levels[i]['l'])
+        
+        levels[i]['s']=(levels[i]['mult']-1)/2
+        
+        if(levels[i]['mult'] > 1):
+            sLbl = str(levels[i]['n']) + '^' + str(levels[i]['mult']) + termSymbols[levels[i]['l']]
+            haveLevel = False
+            for j in range(0,len(splittings)):
+                if(splittings[j]['label'] == sLbl):
+                    splittings[j]['levels'].append({'j' : levels[i]['j'], 'energy' : levels[i]['energy']})
+                    haveLevel = True
+                    break
+            if(not haveLevel):
+                splittings.append({'label' : sLbl, 'levels' : [{'j' : levels[i]['j'], 'energy' : levels[i]['energy']}] })
+        
+        levels[i]['xstart']=xstart
+        minX=min(xstart,minX)
+        maxX=max(xstart,maxX)
+        
+        if(levels[i]['mult'] not in multiplicities):
+            multiplicities.append(levels[i]['mult'])
+    else:
+        labelError = True
+        levels[i]['xstart']=1
+        print("Invalid level label encountered. Will not parse state properties")
+
+##Remove level splittings containing only one level
+count = 0
+while(count < len(splittings)):
+    if(len(splittings[count]['levels']) <= 1):
+        del splittings[count]
+    else:
+        count = count+1
+        
+if(labelError):
+    for lvl in levels:
+        lvl['xstart']=1
+        
 
 multiplicities.sort()
 rangeX = maxX-minX
-for l in levels:
-    l['xstart']+=(rangeX + 1)*multiplicities.index(l['mult'])
-    maxX=max(maxX,l['xstart'])
+if(not labelError):
+    for l in levels:
+        l['xstart']+=(rangeX + 1)*multiplicities.index(l['mult'])
+        maxX=max(maxX,l['xstart'])
     
     
 if(showElectricDipole):
@@ -154,6 +185,7 @@ if(showElectricQuadrupole):
 def nmString(transition):
     return str(int(1239.8393589807376/abs(levels[trans['i']]['energy'] - levels[trans['f']]['energy']))) + 'nm'
 
+##Append wavelength calculation to transition strings, if option set
 for trans in transitions:
     if(not trans.has_key('label')):
         if(trans.has_key('show-nm') and not trans['show-nm']):
@@ -163,10 +195,16 @@ for trans in transitions:
     elif(trans.has_key('show-nm') and trans['show-nm']):
         trans['label']=trans['label'] + ' (' + nmString(trans) +')'
 
+##Plot level lines
 for level in levels:
     plt.plot([level['xstart']-levelWidth,level['xstart']+levelWidth],[level['energy'],level['energy']],'-0')
-for level in levels:
-    plt.annotate('$'+level['label'].split('_')[0]+ '_{'+ str(level['j']) + '}$',xy=(level['xstart']-levelWidth-labelSpacing,level['energy']))
+    aPos = (level['xstart']-levelWidth-labelSpacing,level['energy'])
+    if(not labelError):
+        plt.annotate('$'+level['label'].split('_')[0]+ '_{'+ str(level['j']) + '}$',xy=aPos)
+    else:
+        plt.annotate(level['label'],xy=aPos)
+
+##Draw Transition arrows    
 for t in transitions:
     x=levels[t['i']]['xstart']
     y=levels[t['i']]['energy']
@@ -186,7 +224,10 @@ plt.ylim(minY-yMargin*yRange,maxY+yMargin*yRange)
 plt.ylabel(scale)
 plt.title(title)
 plt.xlim(0,5)
-plt.xticks(range(1,6*len(multiplicities)),(termSymbols[0:maxl+1]+[''])*len(multiplicities))
+if(not labelError):
+    plt.xticks(range(1,6*len(multiplicities)),(termSymbols[0:maxl+1]+[''])*len(multiplicities))
+else:
+    plt.xticks([1],[''])
 for i in range(0,len(multiplicities)-1):
     plt.plot([5*(i+1),5*(i+1)],[minY-yMargin*yRange,maxY+yMargin*yRange],'-0')
 plt.show()
